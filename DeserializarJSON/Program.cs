1bb3a25a8c8be95ce228b268fc78d331e3b42203
Gustavo <lgusta333@gmail.com>
2022-01-23 16:10:05 -0300

    Deserialization Created

---
+++ DeserializarJSON/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web.Script.Serialization;
+using Library;
+    
+namespace DeserializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = new StreamReader(@"C:\Users\Gustavo\Documents\Arquivos c#\02_SerializarJSON_json");
+            string path = stream.ReadToEnd();
+
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            Usuario user = (Usuario) jsSerializer.Deserialize(path, typeof(Usuario));
+
+            Console.WriteLine("User Name: {0}, CPF: {1}, Email: {2}", user.Name, user.CPF, user.Email);
+            Console.ReadKey();
+        }
+    }
+}
